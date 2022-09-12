96ad2a137c72d29e0404bf391cc35ce27f3b8c78
Kenneth Kamau <kennt0990@gmail.com>
2022-09-11 23:17:21 -0700

    use puts

--- 4-puts.c
+++ 4-puts.c
@@ -1,2 +1,11 @@
-#!/bin/bash
-puts('"Programming is like building a multilingual puzzle'\n); return(0);
+#include <stdio.h>
+/**
+ *  *main -Entry point
+ *   *
+ *    *Return: 0 after printing the function
+ *     */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
