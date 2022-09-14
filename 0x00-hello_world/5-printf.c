ba6145c2f1e85b9cc66e84d954d3042db35a7c33
Kenneth Kamau <kennt0990@gmail.com>
2022-09-14 05:20:08 -0700

    printfl

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
 /**
-*main - with proper grammar, but the outcome is a piece of art,
-*return - value zer 0
+* main - with proper grammar, but the outcome is a piece of art,
+* description - print using the main function
+* Return: 0
 */
 
-int main (void)
+int main(void)
 {
 
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+	return (0);
 }
