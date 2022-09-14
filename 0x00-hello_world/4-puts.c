f8e24fd9b81c67b1484f61da74b95f02561e7d42
Kenneth Kamau <kennt0990@gmail.com>
2022-09-14 04:42:43 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
- *
- * print with new line and return zero
- *
+ * print using puts and followed by a new line
+ * end programme with value 0
  **/
-int main (void){
+int main (void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return(0);
 }
