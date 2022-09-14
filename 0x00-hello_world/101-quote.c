976da10a9bcce1cd0a067acec1dbdc7bc74e2d45
Kenneth Kamau <kennt0990@gmail.com>
2022-09-14 05:45:34 -0700

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,7 +3,7 @@
 
 /**
  * main: print and that piece of art is useful\" - Dora Korpar, 2015-10-19 
- * followed by a new line, to the standard error.
+ *Description: followed by a new line, to the standard error.
  * Return: 1
  */
 int main(void)
@@ -11,4 +11,3 @@ int main(void)
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
-
