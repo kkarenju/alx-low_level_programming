9108e72b7cd85b2c326846ee929cab69f43d1072
Kenneth Kamau <kennt0990@gmail.com>
2022-09-14 04:36:41 -0700

    use printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+*print - with proper grammar,
+*but the outcome is a piece of art,
+*/
+
+public main (void)
+{
+
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return(0);
+}
