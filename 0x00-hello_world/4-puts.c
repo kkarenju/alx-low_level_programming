08375c48e3bea5ccc70bd5534961f158e292ea6e
Kenneth Kamau <kennt0990@gmail.com>
2022-09-14 04:17:07 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,6 @@
-#!/bin/bash
-puts("\"Programming is like building a multilingual puzzle"\n); return(0);
+#include <stdio.h>
+
+public main (void){
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return(0);
+}
