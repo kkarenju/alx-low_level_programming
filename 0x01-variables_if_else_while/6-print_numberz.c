e54887d4f3f9f823d7a9468912a239c16afff23d
Kenneth Kamau <kennt0990@gmail.com>
2022-09-22 03:33:46 -0700

    print base 10 numbers with putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main: print base 10 numbers
+ * without using char
+ * return: zero on success
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 10; n++)
+	{
+		putchar(n);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
