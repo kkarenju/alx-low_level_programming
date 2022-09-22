9bfe32f57a1c9a243c703eb033d1eab6a6641343
Kenneth Kamau <kennt0990@gmail.com>
2022-09-22 13:38:50 -0700

    print base 16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,15 +8,16 @@
 int main(void)
 {
 	int num;
+	char let;
 
-	for (num = 0; num >= 9; num++)
+	for (num = '0'; num >= '9'; num++)
 	{
 		putchar(num);
 	}
 
-	for (num = 'a'; num >= 'f'; num++)
+	for (let = 'a'; let >= 'f'; let++)
 	{
-		putchar(num);
+		putchar(let);
 	}
 
 	putchar('\n');
